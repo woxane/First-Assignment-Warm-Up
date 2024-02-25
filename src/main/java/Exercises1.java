ada7abf5cef78d7367a6f900533496ea17e9d53d
woxane <mohsen.minavand1384@gmail.com>
2024-02-25 21:36:38 +0330

    Implement fibonacci function

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -8,13 +8,20 @@ public class Exercises1 {
     }
 
 
-    /*
-    implement a function that return nth number of fibonacci series
-    the series -> 1, 1, 2, 3, 5, 8, ...
-    */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            long a = 1, b = 1, result = 0;
+            for (int i = 3; i <= n; i++) {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+            return result;
+        }
     }
 
 
@@ -40,6 +47,8 @@ public class Exercises1 {
 
         // Test factorial
         System.out.println("Factorial of 5: " + exercises.factorial(5));
+        // Test fibonacci
+        System.out.println("Fibonacci of 7: " + exercises.fibonacci(7));
 
     }
 
